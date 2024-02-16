03dfb0b86ad1d9b893c1eb6454a571b2c32f2c4a
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-02-16 20:14:16 +0300

    implement all documents

--- src/main/java/com/ndrianja/mongotest/project/Project.java
+++ src/main/java/com/ndrianja/mongotest/project/Project.java
@@ -1,11 +1,17 @@
 package com.ndrianja.mongotest.project;
 
 import com.ndrianja.mongotest.user.User;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-
+@Document(collection = "projects")
+@Data
+@Builder
 public class Project {
-
+    @Id
     private String id;
     private String description;
     private Status status;
--- src/main/java/com/ndrianja/mongotest/user/User.java
+++ src/main/java/com/ndrianja/mongotest/user/User.java
@@ -1,8 +1,15 @@
 package com.ndrianja.mongotest.user;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "users")
+@Data
+@Builder
 public class User {
-
+    @Id
     private String id;
     private String firstname;
     private String lastname;
