e5196c5b214d486fca0c86bcccfd7dc4fd31079f
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-02-20 17:04:55 +0300

    fix authorization route for OpenApi (Swagger)

--- src/main/java/com/ndrianja/mongotest/security/AppSecurityConfig.java
+++ src/main/java/com/ndrianja/mongotest/security/AppSecurityConfig.java
@@ -52,6 +52,7 @@ public class AppSecurityConfig {
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth-> auth
                         .requestMatchers("/api/v1/error**","/api/v1/confirm-email","/api/v1/register**","/api/v1/login**","/api/v1/verifyTotp**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/webjars/**").permitAll()
                         .requestMatchers("/api/v1/**").authenticated()
                         .anyRequest().denyAll()
                 )
